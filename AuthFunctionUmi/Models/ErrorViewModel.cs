7bd3bec66f7e238b3c1e7315f42d695e4e767f5d
NoisKung <supakit021@gmail.com>
2025-02-20 10:07:06 +0700

    First Commit

---
+++ AuthFunctionUmi/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AuthFunctionUmi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
